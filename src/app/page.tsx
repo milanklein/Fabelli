db208a15ab80e7c569a3d6ca345533fe64a0d02b
milanklein <milkle005@gmail.com>
2026-07-11 14:52:28 +0200

    test 10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
         {/* <LogoStrip />
         <IntroStatement /> */}
         {/* <Testimonials /> */}
-        {/* <ProcessIntro />*/}
+        <ProcessIntro />
         <ProcessTimeline />
         <ComparisonTable /> 
         {/* <PortfolioGrid /> */}
